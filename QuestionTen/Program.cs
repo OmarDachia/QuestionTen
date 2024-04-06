12b24d26e19e11bd95076ee09d29a0304d664814
Omar Dachia <omardachia@gmail.com>
2024-04-06 08:46:55 +0100

    #9 WAP to swap 2 no’s without using a third variable

---
+++ QuestionTen/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionTen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("#9 WAP to swap 2 no’s without using a third variable.");
+            Console.WriteLine("=============================================");
+            Console.WriteLine("User Input the two numbers:->");
+            Console.WriteLine("+++++++++++++++++++++++++++++");
+
+
+            double first_number = 0, second_number = 0;
+
+            //Get the two number form our console 
+            Console.WriteLine("Enter First Number:->");
+            first_number = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Second Number:->");
+            second_number = Convert.ToDouble(Console.ReadLine());
+
+
+            //Result:->
+            Console.WriteLine("Result:->");
+            Console.WriteLine("+++++++++");
+            Console.WriteLine("BEFORE SWAP");
+            Console.WriteLine("===========");
+
+            Console.WriteLine(string.Format("First Number: {0}, Second Number {1}", first_number, second_number));
+
+            first_number = first_number + second_number;
+            second_number = first_number - second_number;
+            first_number = first_number - second_number;
+
+            Console.WriteLine("AFTER SWAP");
+            Console.WriteLine("==========");
+
+            Console.WriteLine(string.Format("First Number: {0}, Second Number {1}", first_number, second_number));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+#9 WAP to swap 2 no’s without using a third variable.
+Let us see how this program works:
+
+Initially, a = 5 and b = 10.
+Then, we add a and b and store it in a with the code a = a + b. This means a = 5 + 10. So, a = 15 now.
+Then we use the code b = a - b. This means b = 15 - 10. So, b = 5 now.
+Again, we use the code a = a - b. This means a = 15 - 5. So finally, a = 10.
+Hence, the numbers have been swapped.
+
+Note: We can use multiplication and division instead of addition and subtraction. However, this won't work if one of the numbers is 0.
